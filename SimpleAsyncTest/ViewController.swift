607f700d7b169297beaa9ddf9b025756fdae08ef
Adrian Tineo <adrian.tineo@scout24.ch>
2018-02-22 16:23:54 +0100

    Initial Commit

---
+++ SimpleAsyncTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimpleAsyncTest
+//
+//  Created by Tineo Adrian on 22.02.18.
+//  Copyright © 2018 Tineo Adrian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
